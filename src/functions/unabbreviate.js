144e59cc30b3fc71df0e5f47f0e903b7cef6e114
Coelhinho <96489851+Coelhinho10@users.noreply.github.com>
2025-02-26 10:18:38 -0300

    Create unabbreviate.js

---
+++ src/functions/unabbreviate.js
@@ -0,0 +1,33 @@
+export default function unabbreviate(number) {
+  if (!number) {
+    throw new Error("[ZyraPlus] - The parameter <number> must be defined!");
+  }
+
+  if (typeof number !== "string") {
+    throw new Error("[ZyraPlus] - The parameter <number> must be a string!");
+  }
+
+  const symbols = {
+    k: 1e3,
+    M: 1e6,
+    B: 1e9,
+    T: 1e12,
+    Qa: 1e15,
+    Qi: 1e18,
+    Sx: 1e21,
+    Sp: 1e24,
+    Oc: 1e27,
+    Nn: 1e30,
+    Dc: 1e33,
+  };
+
+  const match = number.match(/^([\d.]+)([a-zA-Z]*)$/);
+  if (!match) {
+    throw new Error("[ZyraPlus] - Invalid format for <number>!");
+  }
+
+  const value = parseFloat(match[1]);
+  const suffix = match[2];
+
+  return suffix in symbols ? value * symbols[suffix] : value;
+}
